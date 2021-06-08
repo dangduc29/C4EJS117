51b16ab994adeb944e5df754230c8ee60249b7a2
dangduc29 <85496571+dangduc29@users.noreply.github.com>
2021-06-08 13:37:08 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,135 @@
+
+// //Bài tập homework:
+//         //Câu 1:
+//         //VD 1:
+//         {        let a = 5, b = 6;
+//         [a, b] = [b, a];
+//         console.log(`${a} ${b}`);
+
+//         };
+//         //VD 2:
+//         {
+//         let a = 6, b = 5;
+//         a = c ;
+//         b = a ;
+//         b = c ;
+//         console.log (a, b); 
+// }
+// //Bài 2:
+//         { const s = 'Hello , beauty , there';
+//          const a = s.split(', ');
+//          console.log( a );
+//         }
+// //Bài 3:
+//         { const a = [4, 5, 7, -8];
+//         console.log(...a); }       
+//Bài 4; 
+                // let shop = ['Jeans', 'T-Shirt', 'Socks'];
+                // let init;
+                // let item, index;
+                // while(true){
+                // init = prompt('Hi there, welcome to shop admin panel, what do you want (C, R, U, D)?');
+                // if(init == undefined){
+                //         alert("Thanks");
+                //         break;
+                // }
+                // else if(init == "c"){
+                //         item = prompt('Enter the name of the new item');
+                //         shop.push(item);
+                //         alert("Done");
+                // }
+                // else if(init == "r"){
+                //         for(let i = 0; i < shop.length; i++) console.log((i + 1) + ". " + shop[i]);
+                //         break;
+                
+                // }
+                // else if(init == "u"){
+                //         index = Number(prompt("Enter the position"));
+                //         item = prompt('Enter the name of the new item');
+                //         shop[index - 1] = item;
+                //         alert("Done");
+                // }
+                // else if(init == "d"){
+                //         index = Number(prompt("Enter the position"));
+                //         shop.splice(index-1, 1);
+                //         alert("Done");
+                // }
+                
+                // else{
+                //         alert("This command is not supported");
+                // }
+                // }        
+//bài 5
+        //  {      let x = [], sum = 0;
+        //         let y = prompt("Enter a squence of Number, separated by commas (,)");
+        //         x = y.split(",");
+        //         for(let i = 0; i < x.length; i++) sum += Number(x[i]);
+        //         alert(`The sum of them is ${sum}`);
+        // }
+// //Bài 6:
+                // let a = [], b = 0, min;
+
+                // let init = prompt("Enter a squence of Number, separated by commas (,)");
+
+                // a = init.split(",");
+
+                // min = Number(a[0]);
+                // for(let i = 1; i < a.length; i++){
+                //    if(Number(a[i]) < min) min = Number(a[i]);
+                // }
+                // alert(`The smallest number is ${min}`);
+// //Bài 7:
+        //          const a = [23,34,45,56,67,78];
+        //          let b = Number(prompt('Nhập số cần tìm'));
+        //          for(let i = 0;i<a.length;i++);
+        //          if(b == a[i]){{
+        //         alert('{b} đã tìm thấy trong mảng của tôi'+ i );
+        //         console.log('không có số bạn cần trong mảng của tôi');
+        //          } break;}
+        //         else {
+        //                 b(Number(prompt)('NHập số cần tìm'));
+        //                 alert('Số bạn cần là' + b);
+        //                 break;
+                        
+
+        //      {  { const x = [3, 4, 6, -9, 10, -88, 2];
+        //         let a = 0;
+        //         let b = Number(prompt('Nhập số bạn cần tìm' ));
+        //         for (i = 0;i < x.length;i++);
+        //         if (b = x[i]);
+        //          {
+        //                 alert('${ b } Số bạn cần + x');
+        //         }}
+        //         if 
+                 
+        //                 (alert (b != 1)('{ b } Không tìm thấy')
+                
+        //      }
+
+
+//Bài 8:
+        //        { let cuu = [5, 7, 300, 90, 24, 50, 75];
+        //         console.log('Hello, here is my sheep sizes');           
+        //         console.log(...cuu);
+        //         console.log('Now my biggest sheep has size ' + Math.max(...cuu));
+        //         console.log('After shearing, here is my flock');
+        //         cuu[cuu.indexOf(Math.max(...cuu))] = 8;
+        //         console.log(...cuu);
+        //         let a;
+        //                 for (a = 0; a < cuu.length; a++)cuu[a] += 50;              
+        //         console.log("After one month\n");
+        //         console.log(...cuu);
+        //         let b = Number(prompt('nhập số tháng'));
+        //         let c = 1;
+        //         while(c <= b) {
+        //                 for (a = 0; a < cuu.length; a++)cuu[a] += 50;              
+        //         console.log("After one month\n");
+        //         console.log(...cuu);
+        //         }
+                            
+        // }
+        //         let sum = 0;
+        //         for(let i = 0;i < cuu.length;i++)sum + cuu[i];
+        //         console.log(`Total size: ${sum}`);
+        //         console.log("I would get " + sum*2);
+             
